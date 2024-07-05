f3796d0652b448269b1eccb5ba2bf3e901e8fb16
Minh Nguyễn <nguyenminh168203@gmail.com>
2024-07-05 16:17:50 +0700

    Add project files.

---
+++ QuanLySach/frm_DangNhap.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace QuanLySach
+{
+    partial class frm_DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.btn_DangNhap = new System.Windows.Forms.Button();
+            this.txt_MatKhau = new System.Windows.Forms.TextBox();
+            this.lbl_MatKhau = new System.Windows.Forms.Label();
+            this.txt_TenTaiKhoan = new System.Windows.Forms.TextBox();
+            this.lbl_TenTaiKhoan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.Location = new System.Drawing.Point(474, 306);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(137, 37);
+            this.btn_Thoat.TabIndex = 22;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // btn_DangNhap
+            // 
+            this.btn_DangNhap.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DangNhap.Location = new System.Drawing.Point(224, 306);
+            this.btn_DangNhap.Name = "btn_DangNhap";
+            this.btn_DangNhap.Size = new System.Drawing.Size(137, 37);
+            this.btn_DangNhap.TabIndex = 21;
+            this.btn_DangNhap.Text = "Đăng Nhập";
+            this.btn_DangNhap.UseVisualStyleBackColor = true;
+            this.btn_DangNhap.Click += new System.EventHandler(this.btn_DangNhap_Click);
+            // 
+            // txt_MatKhau
+            // 
+            this.txt_MatKhau.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MatKhau.Location = new System.Drawing.Point(336, 194);
+            this.txt_MatKhau.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_MatKhau.Name = "txt_MatKhau";
+            this.txt_MatKhau.PasswordChar = '*';
+            this.txt_MatKhau.Size = new System.Drawing.Size(254, 33);
+            this.txt_MatKhau.TabIndex = 20;
+            // 
+            // lbl_MatKhau
+            // 
+            this.lbl_MatKhau.AutoSize = true;
+            this.lbl_MatKhau.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MatKhau.Location = new System.Drawing.Point(189, 202);
+            this.lbl_MatKhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_MatKhau.Name = "lbl_MatKhau";
+            this.lbl_MatKhau.Size = new System.Drawing.Size(95, 25);
+            this.lbl_MatKhau.TabIndex = 19;
+            this.lbl_MatKhau.Text = "Mật khẩu";
+            // 
+            // txt_TenTaiKhoan
+            // 
+            this.txt_TenTaiKhoan.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_TenTaiKhoan.Location = new System.Drawing.Point(336, 107);
+            this.txt_TenTaiKhoan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_TenTaiKhoan.Name = "txt_TenTaiKhoan";
+            this.txt_TenTaiKhoan.Size = new System.Drawing.Size(254, 33);
+            this.txt_TenTaiKhoan.TabIndex = 18;
+            // 
+            // lbl_TenTaiKhoan
+            // 
+            this.lbl_TenTaiKhoan.AutoSize = true;
+            this.lbl_TenTaiKhoan.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TenTaiKhoan.Location = new System.Drawing.Point(189, 115);
+            this.lbl_TenTaiKhoan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_TenTaiKhoan.Name = "lbl_TenTaiKhoan";
+            this.lbl_TenTaiKhoan.Size = new System.Drawing.Size(132, 25);
+            this.lbl_TenTaiKhoan.TabIndex = 17;
+            this.lbl_TenTaiKhoan.Text = "Tên tài khoản";
+            // 
+            // frm_DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btn_DangNhap);
+            this.Controls.Add(this.txt_MatKhau);
+            this.Controls.Add(this.lbl_MatKhau);
+            this.Controls.Add(this.txt_TenTaiKhoan);
+            this.Controls.Add(this.lbl_TenTaiKhoan);
+            this.Name = "frm_DangNhap";
+            this.Text = "frm_DangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.Button btn_DangNhap;
+        private System.Windows.Forms.TextBox txt_MatKhau;
+        private System.Windows.Forms.Label lbl_MatKhau;
+        private System.Windows.Forms.TextBox txt_TenTaiKhoan;
+        private System.Windows.Forms.Label lbl_TenTaiKhoan;
+    }
+}
